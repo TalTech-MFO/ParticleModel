5a12536e7ac6825f9172a4eaa744e9fc2116f2f0
Enriko <enriko.siht11@gmail.com>
2023-02-28 16:37:09 +0200

    Test cases

--- test_cases/eddy/plot_trajectory.py
+++ test_cases/eddy/plot_trajectory.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 print("Plotting trajectory...")
 
 df = xr.open_dataset("./out/eddy.out.nc")
-currents = xr.open_dataset("./data/data/eddy.nc")
+currents = xr.open_dataset("./data/data/eddy_2d.nc")
 topo = xr.open_dataset("./data/eddy_topo.nc")
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.quiver(topo.lon[::10], topo.lat[::10],
-          currents.u[0, 0, ::10, ::10], currents.v[0, 0, ::10, ::10])
+          currents.u[0, ::10, ::10], currents.v[0, ::10, ::10])
 # Plot every 5th particle
 ax.plot(df.isel(particle=slice(0, None, 5)).lon, df.isel(
     particle=slice(0, None, 5)).lat, 'go-', linewidth=0.5, markersize=0.5)
